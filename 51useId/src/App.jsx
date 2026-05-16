8983cb264298350c160a98efb42978584dd84c8d
Harshil Khatri <khatriharshil26@gmail.com>
2026-05-16 18:27:33 +0530

    modification of useId hook in react.

--- 51useId/src/App.jsx
+++ 51useId/src/App.jsx
@@ -1,7 +1,13 @@
 import LoginForm from "./components/LoginForm";
+import MultipleId from "./components/MultipleId";
 
 function App() {
-  return <LoginForm />;
+  return (
+    <>
+      <LoginForm />
+      <MultipleId />
+    </>
+  );
 }
 
 export default App;
